d177e9ddef50dce8c87758b7eed348043a75840c
DevpriyaSaini <devpriyasaini6@gmail.com>
2025-11-02 17:34:45 +0530

    config udated

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -12,16 +12,13 @@ export const config = {
 export async function middleware(request: NextRequest) {
   const token = await getToken({ 
     req: request,
-    secret: process.env.NEXT_SECRET! // Changed from NEXT_SECRET to NEXTAUTH_SECRET
+    secret: process.env.NEXT_SECRET! 
   });
   
 
   const { pathname } = request.nextUrl;
 
-  // 1. Redirect authenticated users away from auth pages to dashboard
 
-
-  // 2. Protect admin routes
   if (pathname.startsWith('/dashboard') && !token) {
     if (!token) {
       const signInUrl = new URL('/#pricing', request.url);
